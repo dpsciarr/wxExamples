b4837f6fe8e9997ff1b45dada1caf79401b270aa
dpsciarr <dpsciarr@buffalo.edu>
2021-03-11 18:12:07 -0500

    Add project files.

---
+++ wxExample1/wxExample1/App.cpp
@@ -0,0 +1,27 @@
+#include "App.h"
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+App::App() : wxFrame((wxWindow*)NULL, wxID_ANY, "My First Application", wxPoint(30, 30), wxSize(800, 600)) {
+    // Main Parent Panel
+    wxPanel* mainPanel = new wxPanel(this, wxID_ANY);
+    mainPanel->SetBackgroundColour(wxColor("blue"));
+
+    // Create a Secondary Panel
+    wxPanel* secPanel = new wxPanel(mainPanel, wxID_ANY);
+    secPanel->SetBackgroundColour(wxColor("orange"));
+
+    // Create the main panel sizer and add the secondary panel to it
+    wxBoxSizer* mainPanelSizer = new wxBoxSizer(wxVERTICAL);
+    mainPanelSizer->Add(secPanel, 1, wxEXPAND | wxALL, 20);
+
+    // Set up the main panel sizer for the main panel
+    mainPanel->SetSizer(mainPanelSizer);
+
+}
+
+App::~App() {
+
+}
